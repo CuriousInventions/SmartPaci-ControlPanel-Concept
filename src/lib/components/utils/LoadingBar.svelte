5c7c5642ced733c784898cdb1683aaf01640bda5
Violet Spectra <spectralpegasus@gmail.com>
2024-02-12 17:23:47 +1300

    Add gradient to loading bar

--- src/lib/components/utils/LoadingBar.svelte
+++ src/lib/components/utils/LoadingBar.svelte
@@ -3,9 +3,15 @@
 </script>
 
 <div class="w-full bg-slate-300 rounded-full h-3 relative overflow-hidden">
-	<div class="bg-gradient-to-r from-blue-600 via-green-600 to-amber-600 h-3 rounded-full"></div>
+	<div class="loader-gradient h-3 rounded-full"></div>
 	<div
 		class="absolute top-0 right-0 bottom-0 bg-slate-300 transition-all duration-300"
 		style="width: {100 - width}%;"
 	></div>
 </div>
+
+<style>
+	.loader-gradient {
+		background-image: linear-gradient(90deg, #2563eb 0%, #16a34a 50%, #b1ba04 75%, #d97706 100%);
+	}
+</style>
