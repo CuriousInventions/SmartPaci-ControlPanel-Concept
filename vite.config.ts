a411a6ed8e74b66fec3f71bbe5ef0edb35f8b8db
Tequin Dragon <github@tequ.in>
2025-02-10 16:18:11 +1300

    Improve development experience with including workspaces in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, searchForWorkspaceRoot } from 'vite';
 
 export default defineConfig({
 	build: {
 		sourcemap: 'inline',
 	},
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		fs: {
+			allow: [
+				// search up for workspace root
+				searchForWorkspaceRoot(process.cwd()),
+				// For some reason, it's not detecting the paci and mcumgr workspaces.
+				'/packages/',
+			],
+		},
+	},
 });
